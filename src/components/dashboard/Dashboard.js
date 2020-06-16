f47373fba755c86b946252b98a00f80331c798f6
mumu2117 <jmillerarcher@me.com>
2020-06-16 09:01:47 -0400

    created redux store, reducers

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Notifications from './Notifications'
 import ProjectList from '../projects/ProjectList'
-
+import { connect } from 'react-redux'
 
  class Dashboard extends Component {
     render() {
@@ -23,4 +23,9 @@ import ProjectList from '../projects/ProjectList'
     }
 }
 
-export default Dashboard
+const mapStateToProps = (state) => {
+    return {
+    projects: state.project.projects
+    }
+}
+export default connect(mapStateToProps, null)(Dashboard)

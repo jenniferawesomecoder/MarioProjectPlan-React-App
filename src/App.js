50ae51b7219329c5aa0da76a35df199fd74b6e15
mumu2117 <jmillerarcher@me.com>
2020-06-15 21:17:24 -0400

    created sign in and signup auth components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 
 export default class App extends Component {
@@ -15,8 +17,9 @@ export default class App extends Component {
           <Switch>
 
             <Route exact path='/' component={Dashboard} />
-            <Route path='/project/:id' compoment={ProjectDetails} />
-          
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>

1a820744af1882caff0a619e1aa9281b25733d6d
mumu2117 <jmillerarcher@me.com>
2020-06-20 20:46:40 -0400

    deployed the app. Woohoo! Its live now

---
+++ dist/precache-manifest.099bcc0cce120895900aea1025d4ad8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4abdbf17261a1bcd013a70230de2bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "730c7692af0c4e214843",
+    "url": "/static/css/main.20201272.chunk.css"
+  },
+  {
+    "revision": "9115c7d572b8e8e45080",
+    "url": "/static/js/2.92cc32ef.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/static/js/2.92cc32ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "730c7692af0c4e214843",
+    "url": "/static/js/main.e05ef83a.chunk.js"
+  },
+  {
+    "revision": "98a629c2f8b7a0ef59a4",
+    "url": "/static/js/runtime-main.0d165b64.js"
+  }
+]);
